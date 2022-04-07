cf54c62c3416a58ffa538665feae7911d507a76a
Akeem <AkeemS11788@gmail.com>
2022-04-06 20:53:46 -0400

    initial commit, building backend

---
+++ db/migrate/20220407003419_create_entries.rb
@@ -0,0 +1,11 @@
+class CreateEntries < ActiveRecord::Migration[7.0]
+  def change
+    create_table :entries do |t|
+      t.string :meal_type
+      t.string :description
+      t.integer :calories
+
+      t.timestamps
+    end
+  end
+end
