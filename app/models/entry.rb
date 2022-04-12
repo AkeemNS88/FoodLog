fd51b1ff2ba83d2ddcfaec16bb37a549519eb4b0
Mike <mjpj@live.com>
2022-04-12 16:45:39 -0400

    archives added

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,8 @@
 class Entry < ApplicationRecord
-end
+    
+    validates :description, :calories, :protein, :carbohydrates, :fats, :meal_type, presence: true
+  
+    def day
+      self.created_at.strftime("%b %e, %Y")
+    end
+  end
